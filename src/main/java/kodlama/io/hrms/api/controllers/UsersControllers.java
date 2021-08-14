9a13dc5edb6806403ae5cf7770744e286640a67c
UurZer <46075903+UurZer@users.noreply.github.com>
2021-08-14 15:00:35 +0300

    CrossOrigin Annotation Added

--- src/main/java/kodlama/io/hrms/api/controllers/UsersControllers.java
+++ src/main/java/kodlama/io/hrms/api/controllers/UsersControllers.java
@@ -3,6 +3,7 @@ package kodlama.io.hrms.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,6 +17,7 @@ import kodlama.io.hrms.entities.concretes.User;
 
 @RestController
 @RequestMapping("api/users")
+@CrossOrigin
 public class UsersControllers {
 
 	@Autowired
