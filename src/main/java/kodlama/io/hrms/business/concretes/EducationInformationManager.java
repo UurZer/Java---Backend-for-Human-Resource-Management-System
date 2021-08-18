c3d60323090be0e900cede3db6dbfb19ccdc1d91
UurZer <46075903+UurZer@users.noreply.github.com>
2021-08-18 21:27:18 +0300

    GetAll Added

--- src/main/java/kodlama/io/hrms/business/concretes/EducationInformationManager.java
+++ src/main/java/kodlama/io/hrms/business/concretes/EducationInformationManager.java
@@ -1,10 +1,14 @@
 package kodlama.io.hrms.business.concretes;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlama.io.hrms.business.abstracts.EducationInformationService;
+import kodlama.io.hrms.core.utilities.results.DataResult;
 import kodlama.io.hrms.core.utilities.results.Result;
+import kodlama.io.hrms.core.utilities.results.SuccessDataResult;
 import kodlama.io.hrms.core.utilities.results.SuccessResult;
 import kodlama.io.hrms.dataAccess.abstracts.EducationInformationDao;
 import kodlama.io.hrms.entities.concretes.EducationInformation;
@@ -29,6 +33,12 @@ public class EducationInformationManager implements EducationInformationService 
 		this.educationInformationDao.save(educationInformation);
 		
 		return new SuccessResult("Eğitim bilgisi eklendi.");
+	}
+
+	@Override
+	public DataResult<List<EducationInformation>> GetAll() {
+		
+		return new SuccessDataResult<List<EducationInformation>>(this.educationInformationDao.findAll());
 	}
 
 }
